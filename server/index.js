7e913d2518174ef4c1cc3b5ed066c6197594cd24
Narongpol Kijrangsan <28519551+nkd3v@users.noreply.github.com>
2022-10-12 23:29:35 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://snappy.ml",
+    origin: "https://app.snappy.ml",
     credentials: true,
   },
 });
